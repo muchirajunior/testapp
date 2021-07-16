8ca20dea1597a0d9367cf6a9cc6378b6065e5d60
root <root@localhost.localdomain>
2021-07-16 22:20:53 +0300

    made requirements changes

--- app.py
+++ app.py
@@ -4,29 +4,29 @@ from flask_marshmallow import Marshmallow
 
 app=Flask(__name__)
 
-# app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://muchirajn@muchiradb:junior_12@muchiradb.postgres.database.azure.com:5432/testdb"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
-# app.config["SECRET_KEY"]="34etrgfjkliop8iuyhg"
+app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://muchirajn@muchiradb:junior_12@muchiradb.postgres.database.azure.com:5432/testdb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+app.config["SECRET_KEY"]="34etrgfjkliop8iuyhg"
 
-# db=SQLAlchemy(app)
-# ma=Marshmallow(app)
+db=SQLAlchemy(app)
+ma=Marshmallow(app)
 
-# class Users(db.Model):
-#        id=db.Column(db.Integer,primary_key=True)
-#        name=db.Column(db.String(100))
-#        email=db.Column(db.String(50))
-#        password=db.Column(db.String(20))
+class Users(db.Model):
+       id=db.Column(db.Integer,primary_key=True)
+       name=db.Column(db.String(100))
+       email=db.Column(db.String(50))
+       password=db.Column(db.String(20))
 
-#        def __init__(self,name,email,password):
-#               self.name=name
-#               self.email=email
-#               self.password=password
+       def __init__(self,name,email,password):
+              self.name=name
+              self.email=email
+              self.password=password
 
-# class UserSchema(ma.Schema):
-#        class Meta:
-#               fields=('name','email')
+class UserSchema(ma.Schema):
+       class Meta:
+              fields=('name','email')
 
-# usershema=UserSchema(many=True)
+usershema=UserSchema(many=True)
 
 @app.route("/")
 def index():
@@ -38,27 +38,26 @@ def junior():
   
    return {"route":"junior!!!!!!!!"}
 
-# @app.route("/adduser",methods=["POST"])
-# def adduser():
-#        try:
-#            name=request.json['name']
-#            email=request.json['email']
-#            password=request.json['password']
+@app.route("/adduser",methods=["POST"])
+def adduser():
+       try:
+           name=request.json['name']
+           email=request.json['email']
+           password=request.json['password']
           
-#            newuser=Users(name,email,password)
+           newuser=Users(name,email,password)
 
-#            db.session.add(newuser)
-#            db.session.commit()
+           db.session.add(newuser)
+           db.session.commit()
 
-#            return {"feedback":"success"}
-#        except:
-#            return {"feedback":"failed"}
+           return {"feedback":"success"}
+       except:
+           return {"feedback":"failed"}
 
-# @app.route("/allusers")
-# def allusers():
-#        users=Users.query.all()
-#        users=usershema.dump(users)
+@app.route("/allusers")
+def allusers():
+       users=Users.query.all()
+       users=usershema.dump(users)
 
-#        return usershema.jsonify(users)
+       return usershema.jsonify(users)
  
-#  app.run()
